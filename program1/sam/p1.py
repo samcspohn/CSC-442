f682e36cecbeb3edd3367826601d85d8c975029b
samuel spohn <samuel.c.spohn@gmail.com>
2019-09-19 18:38:32 -0500

    cleaned up code a little. added comments to sams program1

--- program1/sam/p1.py
+++ program1/sam/p1.py
@@ -5,15 +5,16 @@ s = sys.stdin.read() # f.readline()
 hasReturn = 0
 if(s[-1] == '\r' or s[-1] == '\n'):
     hasReturn = 1
-if((len(s) - hasReturn) % 7 == 0):
+lenS = len(s) - hasReturn
+if(lenS % 7 == 0):
     c = 7
-    for i in range(int(len(s) / c)):
-        print(chr(int(s[i*c:i*c + c],2)), end='')
+    for i in range(int(lenS / c)):
+        print(chr(int(s[i*c:i*c + c],2)), end='') # take 7 bit chunks of input string, convert to int, convert to char. print to console. backspace moves cursor back
     print("")
 
-if((len(s)- hasReturn) % 8 == 0):
+if(lenS % 8 == 0):
     c = 8
-    for i in range(int(len(s) / c)):
-        print(chr(int(s[i*c:i*c + c],2)), end='')
+    for i in range(int(lenS / c)):
+        print(chr(int(s[i*c:i*c + c],2)), end='') 
     print("")
     
