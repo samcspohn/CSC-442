d4b776bb92d5c689320b65427935d483ea52de84
M3JPUV <drewalmond@gmail.com>
2019-09-25 13:55:02 -0500

    Adding test file & notes

--- program3/prep4decode.py
+++ program3/prep4decode.py
@@ -1,18 +1,19 @@
 
 METHOD = 7
 
+# open text file and create new file for edited string
 test = 'testFile.txt'
 with open(test) as tf:
     temp = open("tempFile.txt", "a+")
     line = tf.readline()
 
+# read line by line
+# replace "-" with "0" (replacing characters hasn't been implemented yet)
+# strip excess parts off
+
     while line != "":
         strip = line.strip()
         temp.write("{}".format((strip[10-METHOD:10]).replace("-", "0")))
-        print("{}".format(line.strip()))
         line = tf.readline()
 
-    string = ("{}".format(temp))
-
-    print(string)
     temp.close()
